b5ec73ea6b4b348e9858814ecb3c54508ff4b68d
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-01-08 13:54:09 -0500

    first team push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newsItem: null,
+    };
+  }
+  componentDidMount() {
+    console.log("mounted");
+    fetch("http://hn.algolia.com/api/v1/items/1")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ newsItem: data });
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <input type="text" />
+          <button>search</button>
+        </form>
+        <ul>
+          {this.state.newsItem &&
+            this.state.newsItem.map((item) => {
+              return (
+                <item
+                  date={item.created_at}
+                  author={item.author}
+                  title={item.title}
+                />
+              );
+            })}
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
